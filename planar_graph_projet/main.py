2be9f9487de07ccec87a320e81bfc473b489c9ca
noafoucoux <Noa.foucoux@student.umons.ac.be>
2023-06-05 12:00:57 +0200

    renaming

---
+++ planar_graph_projet/main.py
@@ -0,0 +1,42 @@
+from planar_graph_projet.map_to_graph import map_to_graph as m2g
+from planar_graph_generation import graph_generation_np_array
+from planar_graph_projet.map_to_graph.plot_generation import plot_generation_ratio
+import time
+start_time = time.time()
+
+usa = False
+
+#choose the parameters of the graph
+G = graph_generation_np_array.graph_generation()
+
+#plot a graph of a place in the world
+#G= m2g.map_to_graph_from_point([35.7521, 139.8781, 3000], plot=True)
+
+color = ["purple", "red", "green", "yellow", "orange", "blue"]
+tot = [e for e in m2g.get_degree_sequence(G) if e != 2 and e != 1]
+degree = [tot]
+
+if usa == True:
+    style = "USA"
+else:
+    style = "Classic"
+
+txt = "Graph generation with a number of nodes equal to " + str(len(degree[0])) + \
+      ". \nThe style is " + style + "."
+
+for e in range(1, max(tot)):
+
+    if e != 2 and e != 1:
+        txt = txt + "\nThe number of node of degree " + str(e) + " is equal to " + str(tot.count(e)) + ". " + \
+              "The ratio is equal to " + str(round(tot.count(e) / len(tot), 2)) + ". "
+        if e < 5:
+            txt = txt + "The color of the node is " + color[e] + ". "
+        else:
+            txt = txt + "The color of the node is " + color[5] + ". "
+
+print(txt)
+
+plot_generation_ratio(degree, ["test"])
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
